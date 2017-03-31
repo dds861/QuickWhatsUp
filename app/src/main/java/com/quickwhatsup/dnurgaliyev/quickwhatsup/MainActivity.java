d6dcf37674b1ebf2846db5436cb3e875a296280f
dds861 <dds861@Gmail.com>
2017-03-31 11:08:57 +0600

    fixes

--- app/src/main/java/com/quickwhatsup/dnurgaliyev/quickwhatsup/MainActivity.java
+++ app/src/main/java/com/quickwhatsup/dnurgaliyev/quickwhatsup/MainActivity.java
@@ -1,60 +1,90 @@
 package com.quickwhatsup.dnurgaliyev.quickwhatsup;
 
+import android.content.ContentProviderOperation;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.NumberPicker;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button btn;
-    EditText MA_ed_address;
+    EditText etPhoneNumber;
     NumberPicker numberPicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn=(Button)findViewById(R.id.btn1);
-        MA_ed_address=(EditText)findViewById(R.id.MA_ed_address);
+        btn = (Button) findViewById(R.id.btn1);
+        etPhoneNumber = (EditText) findViewById(R.id.etPhoneNumber);
 
-        numberPicker =(NumberPicker)findViewById(R.id.numberPicker) ;
-        numberPicker.setMinValue(1);
-        numberPicker.setMaxValue(10);
-        numberPicker.setWrapSelectorWheel(false);
 
         btn.setOnClickListener(this);
-
 
 
     }
 
     @Override
     public void onClick(View v) {
+//------------------------------------------------------
+        String smsNumber0 = etPhoneNumber.getText().toString();
+        String smsNumber1 = "8" + smsNumber0;
+        ArrayList<ContentProviderOperation> ops = new ArrayList<ContentProviderOperation>();
 
-
-        String smsNumber = "77017774941";
-        Intent sendIntent = new Intent();
+        ops.add(ContentProviderOperation.newInsert(
+                ContactsContract.RawContacts.CONTENT_URI)
+                .withValue(ContactsContract.RawContacts.ACCOUNT_TYPE, null)
+                .withValue(ContactsContract.RawContacts.ACCOUNT_NAME, null)
+                .build());
+//------------------------------------------------------ Mobile Number
+        if (smsNumber1 != null) {
+            ops.add(ContentProviderOperation.
+                    newInsert(ContactsContract.Data.CONTENT_URI)
+                    .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
+                    .withValue(ContactsContract.Data.MIMETYPE,
+                            ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
+                    .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, smsNumber1)
+                    .withValue(ContactsContract.CommonDataKinds.Phone.TYPE,
+                            ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE)
+                    .build());
+        }
+//------------------------------------------------------
         try {
+            getContentResolver().applyBatch(ContactsContract.AUTHORITY, ops);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.d("asd", "Exception: " + e.getMessage());
+            Toast.makeText(this, "Exception: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+//------------------------------------------------------
+        try {
+            Thread.sleep(5000);
+
+            String smsNumber = "7" + smsNumber0;
+//        String smsNumber = "77017774941";
+            Intent sendIntent = new Intent();
+
 
             //sendIntent.setComponent(new ComponentName("com.whatsapp", "com.whatsapp.Conversation"));
             sendIntent.setAction(Intent.ACTION_SEND);
             sendIntent.putExtra(Intent.EXTRA_TEXT,
-                    "Здравствуйте,\n" +
-                            "можно заказать воду.\n" +
-                            "АДРЕС: " + String.valueOf(MA_ed_address.getText())+"\n" +
-                            "БУТЫЛЕЙ: " + String.valueOf(numberPicker.getValue())+"\n" +
-                            "Спасибо.");
+                            " ");
             sendIntent.setType("text/plain");
             sendIntent.putExtra("jid", smsNumber + "@s.whatsapp.net"); //phone number without "+" prefix
             sendIntent.setPackage("com.whatsapp");
             startActivity(sendIntent);
-        } catch(Exception e) {
+        } catch (Exception e) {
             Toast.makeText(this, "Error/n" + e.toString(), Toast.LENGTH_SHORT).show();
         }
+
     }
 }
